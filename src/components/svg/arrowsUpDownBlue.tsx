56227c2afb5d78bf649e5ac1121a9f4fadba71aa
Flame <5017_30@mail.ru>
2023-10-18 02:31:51 +0300

    add: geodata

---
+++ src/components/svg/arrowsUpDownBlue.tsx
@@ -0,0 +1,22 @@
+import { component$ } from '@builder.io/qwik'
+
+export default component$(() => {
+	return (
+		<>
+			<svg
+				xmlns='http://www.w3.org/2000/svg'
+				width='16'
+				height='16'
+				viewBox='0 0 16 16'
+				fill='none'
+			>
+				<path
+					d='M2 5L5 2M5 2L8 5M5 2V11M14 11L11 14M11 14L8 11M11 14V5'
+					stroke='#8EC2FF'
+					stroke-linecap='round'
+					stroke-linejoin='round'
+				/>
+			</svg>
+		</>
+	)
+})
--- src/routes/geodata/index.tsx
+++ src/routes/geodata/index.tsx
@@ -1,12 +1,67 @@
 import { component$ } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
 
+import { Button } from '~/components/button/Button'
 import { Header } from '~/components/header/Header'
+import {
+	SvgArrowsUpDownBlue,
+	SvgArrowsUpDownOrange,
+	SvgChartBar,
+	SvgDownload,
+	SvgUpdate
+} from '~/components/svg'
+
+import s from './Geodata.module.css'
 
 export default component$(() => {
 	return (
 		<>
 			<Header subheading='Геоданные и измерения' />
+			<div class={s.geodata}>
+				<h2 class={s.heading}>
+					GeoData <span>RFC 5870</span>
+				</h2>
+				<div class={s.coords}>geo:54.8143,36.2911?z=14</div>
+				<div class={s.card_block}>
+					<div class={s.row}>
+						<div class={s.card}>
+							<SvgChartBar />
+							<div class={s.text}>
+								Мощность <span>-77 Дб</span>
+							</div>
+						</div>
+						<div class={s.card}>
+							<SvgArrowsUpDownOrange />
+							<div class={s.text}>
+								Задержка <span>~3 ms</span>
+							</div>
+						</div>
+					</div>
+					<div class={s.card}>
+						<SvgArrowsUpDownBlue />
+						<div class={s.text}>
+							До вышки <span>100 м</span>
+						</div>
+					</div>
+				</div>
+				<div class={s.bottom}>
+					<h2 class={s.heading}>
+						GeoAtl <span>CC0</span>
+					</h2>
+					<div class={s.row}>
+						<Button color='#786c5a'>
+							<SvgDownload />
+							Скачать архив
+						</Button>
+						Масштаб
+					</div>
+				</div>
+
+				<Button color='#B462F5'>
+					<SvgUpdate />
+					Обновить
+				</Button>
+			</div>
 		</>
 	)
 })
