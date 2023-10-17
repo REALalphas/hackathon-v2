bb2da0b9f7e42b46762b6b98ff910dc4ba4b1bbf
AlphaS <alphas@alphas.cyou>
2023-10-18 02:04:48 +0300

    animations update and new kostil for nav

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -5,13 +5,14 @@ import { SvgGeodata, SvgMap, SvgSpeed } from '~/components/svg'
 
 import s from './Nav.module.css'
 
+
 export const Nav = component$(() => {
 	const loc = useLocation()
 	const locName = loc.url.pathname.split('/')[1]
 
 	return (
 		<nav class={s.nav}>
-			<Link href='/speed' class={[s.link, locName == 'speed' && s.active]} data-type='speed'>
+			<Link href='/speed' onClick$={(_, el) => el.classList.add(s.active)} class={[s.link, locName == 'speed' && s.active]} data-type='speed'>
 				<div class={s.bg}></div>
 				<div class={s.content}>
 					<SvgSpeed />
@@ -19,7 +20,7 @@ export const Nav = component$(() => {
 				</div>
 			</Link>
 
-			<Link href='/map' class={[s.link, locName == 'map' && s.active]} data-type='map'>
+			<Link href='/map' onClick$={(_, el) => el.classList.add(s.active)} class={[s.link, locName == 'map' && s.active]} data-type='map'>
 				<div class={s.bg}></div>
 				<div class={s.content}>
 					<SvgMap />
@@ -27,7 +28,7 @@ export const Nav = component$(() => {
 				</div>
 			</Link>
 
-			<Link href='/geodata' class={[s.link, locName == 'geodata' && s.active]} data-type='geodata'>
+			<Link href='/geodata' onClick$={(_, el) => el.classList.add(s.active)} class={[s.link, locName == 'geodata' && s.active]} data-type='geodata'>
 				<div class={s.bg}></div>
 				<div class={s.content}>
 					<SvgGeodata />
