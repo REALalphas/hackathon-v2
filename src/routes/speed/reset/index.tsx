d0cd4f3c094272d62711d947157cf80e79aaf82b
AlphaS <ar.su0104@gmail.com>
2024-03-24 07:57:20 +0300

    Update index.tsx

--- src/routes/speed/reset/index.tsx
+++ src/routes/speed/reset/index.tsx
@@ -1,14 +1,6 @@
-import { $, component$, useSignal, useVisibleTask$ } from '@builder.io/qwik'
+import { component$, useVisibleTask$ } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
 import { useNavigate } from '@builder.io/qwik-city'
-import SpeedTest from '@cloudflare/speedtest'
-
-import { Button } from '~/components/button/Button'
-import { Counter } from '~/components/counter/Counter'
-import { Header } from '~/components/header/Header'
-import { SvgArrowDown, SvgArrowUp } from '~/components/svg'
-
-import s from '../Speed.module.css'
 
 export default component$(() => {
 	const nav = useNavigate()
